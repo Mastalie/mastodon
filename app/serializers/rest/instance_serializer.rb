110bbb38c8f0f897848e02326862d725d3c7ec5f
mastalie <yl520@duke.edu>
2021-03-19 10:33:37 +0800

    Update instance settings

--- app/serializers/rest/instance_serializer.rb
+++ app/serializers/rest/instance_serializer.rb
@@ -5,7 +5,7 @@ class REST::InstanceSerializer < ActiveModel::Serializer
 
   attributes :uri, :title, :short_description, :description, :email,
              :version, :urls, :stats, :thumbnail,
-             :languages, :registrations, :approval_required, :invites_enabled
+             :languages, :registrations, :approval_required, :max_toot_chars, :invites_enabled
 
   has_one :contact_account, serializer: REST::AccountSerializer
 
